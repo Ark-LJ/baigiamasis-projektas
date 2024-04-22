d9d91022d6f88fbff899639292d60a12455077ea
Norveli <el.norvaisas@gmail.com>
2024-04-22 13:16:22 +0300

    TEST

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,8 @@ function App() {
   return (
     <div className="App">
       <div className="pages">
-
+        <h1>Eligijus TEST!</h1>
+        <p>Vienas du vienas du</p>
       </div>
     </div>
   );
